27150c3561a5402dc1b707d0409f51bc0a6eae0f
Sylvain Dégué <sylvain.degue@gmail.com>
2020-09-06 13:27:04 -0400

    update test

--- backend/tests/test_api_business.py
+++ backend/tests/test_api_business.py
@@ -1,164 +1,152 @@
 from rest_framework import status
 from rest_framework.reverse import reverse
 from rest_framework.test import APITestCase
+from json import loads, dumps
 
+from main.utils import reverse_querystring
 from users.models import CustomUser
-from ..models import Business, Category, Tag
+from ..models import Business, Category, Tag, BaseModel
 from ..serializers import BusinessSerializer
 
 
-# class TestBusinessEndpoint(APITestCase):
-#     url = reverse("business-list")
-#
-#     def setUp(self):
-#         self.user = CustomUser.objects.create_user(
-#             email="jojo@mail.com", password="bizzare"
-#         )
-#         self.client.force_authenticate(user=self.user)
-#
-#         self.category = Category.objects.create(name="Restaurant")
-#         self.tag = Tag.objects.create(name="africain")
-#         self.tag2 = Tag.objects.create(name="tag2")
-#         self.business = Business.objects.create(
-#             name="gracia afrika", category=self.category
-#         )
-#         self.business2 = Business.objects.create(
-#             name="restaurant2", category=self.category
-#         )
-#         self.business.tags.add(self.tag)
-#         self.business.save()
-#         self.business2.tags.add(self.tag2)
-#
-#     def test_get_all(self):
-#         response = self.client.get(self.url)
-#         businesses = Business.objects.all()
-#         serializer = BusinessSerializer(businesses, many=True)
-#         self.assertEqual(response.data["results"], serializer.data)
-#         self.assertEqual(response.status_code, status.HTTP_200_OK)
-#
-#     def test_get_detail(self):
-#         response = self.client.get(
-#             reverse("business-detail", kwargs={"pk": 1})
-#         )
-#         self.assertEqual(
-#             response.data,
-#             {
-#                 "category": {"id": 1, "name": "Restaurant"},
-#                 "id": 1,
-#                 "name": "gracia afrika",
-#                 "description": "",
-#                 "email": "",
-#                 "slogan": "",
-#                 "status": "pending",
-#                 "tags": [{"id": 1, "name": "africain"}],
-#                 "website": "",
-#                 "deleted_at": None,
-#             },
-#         )
-#         self.assertEqual(response.status_code, status.HTTP_200_OK)
-#
-#     def test_put(self):
-#         response = self.client.put(
-#             reverse("business-detail", kwargs={"pk": 1}),
-#             {
-#                 "id": 1,
-#                 "name": "jojo",
-#                 "category": {"id": 1, "name": "Restaurant"},
-#                 "tags": {"id": 2, "name": "tag2"},
-#             },
-#             format="json",
-#         )
-#         self.assertEqual(
-#             response.data,
-#             {
-#                 "category": {"id": 1, "name": "Restaurant"},
-#                 "id": 1,
-#                 "name": "jojo",
-#                 "description": "",
-#                 "email": "",
-#                 "slogan": "",
-#                 "status": "pending",
-#                 "tags": [{"id": 1, "name": "africain"}],
-#                 "website": "",
-#                 "deleted_at": None,
-#                 "accepted_at": None,
-#             },
-#         )
-#         self.assertEqual(response.status_code, status.HTTP_200_OK)
-#         self.assertEqual("jojo", Business.objects.get(name="jojo").name)
-#
-#     def test_get_with_filter(self):
-#         url = reverse("business-list")
-#         query_param = "name__icontains=res"
-#         full_url = f"{url}?{query_param}"
-#         response = self.client.get(full_url, format="json",)
-#
-#         self.assertEqual(response.status_code, status.HTTP_200_OK)
-#         self.assertEqual(
-#             response.data["results"],
-#             [
-#                 {
-#                     "category": {"id": 1, "name": "Restaurant"},
-#                     "id": 2,
-#                     "name": "restaurant2",
-#                     "description": "",
-#                     "email": "",
-#                     "slogan": "",
-#                     "status": "pending",
-#                     "tags": [{"id": 2, "name": "tag2"}],
-#                     "website": "",
-#                     "deleted_at": None,
-#                 },
-#             ],
-#         )
-#
-#     def test_get_with_filter2(self):
-#         url = reverse("business-list")
-#         query_param = "tags__name__icontains=tag2"
-#         full_url = f"{url}?{query_param}"
-#         response = self.client.get(full_url, format="json",)
-#         self.assertEqual(response.status_code, status.HTTP_200_OK)
-#         self.assertEqual(
-#             response.data["results"],
-#             [
-#                 {
-#                     "category": {"id": 1, "name": "Restaurant"},
-#                     "id": 2,
-#                     "name": "restaurant2",
-#                     "description": "",
-#                     "email": "",
-#                     "slogan": "",
-#                     "status": "pending",
-#                     "tags": [{"id": 2, "name": "tag2"}],
-#                     "website": "",
-#                     "deleted_at": None,
-#                 }
-#             ],
-#         )
+class TestBusinessEndpoint(APITestCase):
+    url = reverse("business-list")
 
-# def test_tags_post(self):
-#     # view = BusinessViewSet.as_view({'get': 'tags'})
-#     response = self.client.post(
-#         reverse('business-tags',args=['1']),
-#         {"id": 2, "name": "tag2"},
-#     )
-#     # factory = APIRequestFactory()
-#     # request = factory.post('/api/business/1/tags', {"id": 2, "name": "tag2"}, format='json')
-#     # response = view(request)
-#     # response.render()
-#     self.assertEqual(
-#         response.data,
-#
-#             # "category": {"id": 1, "name": "Restaurant"},
-#             # "id": 1,
-#             # "name": "gracia afrika",
-#             # "description": "",
-#             # "email": "",
-#             # "slogan": "",
-#             # "status": "pending",
-#              [{"id": 1, "name": "africain"}, {"id": 2, "name": "tag2"}],
-#             # "website": "",
-#
-#
-#     )
-#     self.assertEqual(response.status_code, status.HTTP_200_OK)
+    def setUp(self):
+        self.user = CustomUser.objects.create_user(
+            email="jojo@mail.com", password="bizzare"
+        )
+        self.client.force_authenticate(user=self.user)
+
+        self.category = Category.objects.create(name="Restaurant")
+        self.tag = Tag.objects.create(name="africain")
+        self.tag2 = Tag.objects.create(name="tag2")
+        self.business = Business.objects.create(
+            name="gracia afrika", category=self.category
+        )
+        self.business2 = Business.objects.create(
+            name="restaurant2", category=self.category
+        )
+        self.business.tags.add(self.tag)
+        self.business.save()
+        self.business2.tags.add(self.tag2)
+
+    def test_get_all(self):
+        response = self.client.get(self.url)
+        businesses = Business.objects.all()
+        serializer = BusinessSerializer(businesses, many=True)
+        self.assertEqual(response.data["results"], serializer.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    #
+    def test_get_detail(self):
+        response = self.client.get(
+            reverse("business-detail", kwargs={"pk": 1}), format="json"
+        )
+        self.assertDictEqual(
+            to_dict(response.data),
+            {
+                "id": 1,
+                "category": {"id": 1, "name": "Restaurant"},
+                "name": "gracia afrika",
+                "description": "",
+                "email": "",
+                "slogan": "",
+                "status": "pending",
+                "tags": [{"id": 1, "name": "africain"}],
+                "website": "",
+                "deleted_at": None,
+                "accepted_at": None,
+            },
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    #
+    def test_put(self):
+        response = self.client.put(
+            reverse("business-detail", kwargs={"pk": 1}),
+            {
+                "id": 1,
+                "name": "jojo",
+                "category": {"id": 1, "name": "Restaurant"},
+                "tags": {"id": 2, "name": "tag2"},
+            },
+            format="json",
+        )
+        self.assertDictEqual(
+            to_dict(response.data),
+            {
+                "category": {"id": 1, "name": "Restaurant"},
+                "id": 1,
+                "name": "jojo",
+                "description": "",
+                "email": "",
+                "slogan": "",
+                "status": "pending",
+                "tags": [{"id": 1, "name": "africain"}],
+                "website": "",
+                "deleted_at": None,
+                "accepted_at": None,
+            },
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual("jojo", Business.objects.get(name="jojo").name)
+
+    #
+    def test_search(self):
+        url = reverse_querystring(
+            "business-list", query_kwargs={"search": "res"}
+        )
+        response = self.client.get(url, format="json",)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(
+            to_dict(response.data["results"]),
+            [
+                {
+                    "category": {"id": 1, "name": "Restaurant"},
+                    "id": 2,
+                    "name": "restaurant2",
+                    "description": "",
+                    "email": "",
+                    "slogan": "",
+                    "status": "pending",
+                    "tags": [{"id": 2, "name": "tag2"}],
+                    "website": "",
+                    "deleted_at": None,
+                    "accepted_at": None,
+                },
+            ],
+        )
+
+    def test_tags_put(self):
+        response = self.client.put(
+            reverse_querystring("business-tags", args=["1"]), {"names": "tag2"}
+        )
+        self.assertEqual(
+            to_dict(response.data),
+            [{"id": 1, "name": "africain"}, {"id": 2, "name": "tag2"}],
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_tags_delete(self):
+        response = self.client.delete(
+            reverse_querystring("business-tags", args=["1"]), {"ids": "1,2"}
+        )
+        self.assertEqual(
+            to_dict(response.data), [],
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+def custom_serializer(obj):
+    if isinstance(obj, BaseModel):
+        return obj.__dict__
+    else:
+        # Will get into this if the value is not serializable by default
+        # and is also not a Name class object
+        return None
+
+
+def to_dict(input_ordered_dict):
+    return loads(dumps(input_ordered_dict, default=custom_serializer))
---
+++ main/utils.py
@@ -0,0 +1,26 @@
+from django.utils.http import urlencode
+from rest_framework.reverse import reverse
+
+
+def reverse_querystring(
+    view,
+    urlconf=None,
+    args=None,
+    kwargs=None,
+    current_app=None,
+    query_kwargs=None,
+):
+    """Custom reverse to handle query strings.
+    Usage:
+        reverse('app.views.my_view', kwargs={'pk': 123}, query_kwargs={'search': 'Bob'})
+    """
+    base_url = reverse(
+        view,
+        urlconf=urlconf,
+        args=args,
+        kwargs=kwargs,
+        current_app=current_app,
+    )
+    if query_kwargs:
+        return "{}?{}".format(base_url, urlencode(query_kwargs))
+    return base_url
