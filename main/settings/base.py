b07df13683e5c834c69ceed6137736c355635a42
komlan55 <sylvain.degue@gmail.com>
2021-06-28 19:09:31 -0400

    update settings

--- main/settings/base.py
+++ main/settings/base.py
@@ -15,10 +15,10 @@ import os
 from django.utils.translation import ugettext_lazy as _
 from dotenv import load_dotenv
 
-SECRET_KEY = os.getenv("SECRET_KEY")
-
 project_folder = os.path.expanduser('~/baobab-backend')  # adjust as appropriate
 load_dotenv(os.path.join(project_folder, '.env'))
+
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
